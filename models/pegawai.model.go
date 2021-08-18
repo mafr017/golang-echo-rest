497b1f9fb2cc104b0a985dbf947639d89c8f744f
mafr017 <madityafr3@gmail.com>
2021-08-18 16:20:01 +0700

    implementasi validasi pada method post inser data pegawai

--- models/pegawai.model.go
+++ models/pegawai.model.go
@@ -3,14 +3,15 @@ package models
 import (
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/mafr017/golang-rest-echo/db"
 )
 
 type Pegawai struct {
 	Id int			`json:"id"`
-	Nama string		`json:"nama"`
-	Alamat string	`json:"alamat"`
-	Telepon string	`json:"telepon"`
+	Nama string		`json:"nama" validate:"required"`
+	Alamat string	`json:"alamat" validate:"required"`
+	Telepon string	`json:"telepon" validate:"required"`
 }
 
 func FetchAllPegawai() (Response, error) {
@@ -48,6 +49,19 @@ func FetchAllPegawai() (Response, error) {
 
 func StorePegawai(nama, alamat, telepon string) (Response, error) {
 	var res Response
+
+	v := validator.New()
+	
+	peg := Pegawai{
+		Nama: nama,
+		Alamat: alamat,
+		Telepon: telepon,
+	}
+
+	err := v.Struct(peg)
+	if err != nil {
+		return res, err
+	}
 
 	con := db.CreateConn()
 	
